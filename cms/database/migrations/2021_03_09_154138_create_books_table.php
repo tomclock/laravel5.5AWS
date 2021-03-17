70a16b3257c20d521032f60859ab8bec4dc679af
EC2 Default User <ec2-user@ip-172-31-47-238.ap-northeast-1.compute.internal>
2021-03-17 23:20:11 +0000

    the second edit of source code

--- cms/database/migrations/2021_03_09_154138_create_books_table.php
+++ cms/database/migrations/2021_03_09_154138_create_books_table.php
@@ -19,6 +19,7 @@ class CreateBooksTable extends Migration
             $table->integer('item_number');  
             $table->integer('item_amount');  
             $table->datetime('published'); 
+            $table->bigInteger('user_id'); //Add:user_id
             $table->timestamps();
         });
     }
--- cms/routes/web.php
+++ cms/routes/web.php
@@ -1,34 +1,32 @@
+
 <?php
-
 use App\Book;
-use Illuminate\Http\Request;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request; 
+
+//本ダッシュボード表示
+Route::get('/', 'BooksController@index');
+
+//登録処理
+Route::post('/books','BooksController@store');
+
+//更新画面
+Route::post('/booksedit/{books}','BooksController@edit');
+
+//更新処理
+Route::post('/books/update','BooksController@update');
+
+//本を削除
+Route::delete('/book/{book}','BooksController@destroy');
+
+//Auth
 Auth::routes();
-Route::get('/home','HomeController@index')->name('home');
+Route::get('/home', 'BooksController@index')->name('home'); 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => 'auth'], function () {
+   //welcomeページを表示
+   Route::get("/",function(){
+     return view("welcome");
+   });
 
-/**
- * 新本を作成 
- */
-Route::post('/books', function(){
-    //   
 });
-
-/**
- * 本を削除
- */
-Route::delete('/book/{book}', function(){
-    //   
-});
+#[END]--------------------------------------------

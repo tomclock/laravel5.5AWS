1246446faaadd1418f550058a8c729d7e50e1427
EC2 Default User <ec2-user@ip-172-31-47-238.ap-northeast-1.compute.internal>
2021-03-19 17:00:06 +0000

    add change

--- cms/database/seeds/BooksTableSeeder.php
+++ cms/database/seeds/BooksTableSeeder.php
@@ -13,11 +13,11 @@ class BooksTableSeeder extends Seeder
     {
         //
         $fake = Faker\Factory::create('ja_JP');
-        for($i=0; $i<10;$i++){
+        for($i=0; $i<10; $i++){
             App\Book::create([
             'item_name' => $fake->word(),
-            'item_number' => $fake->numberBetween(1,999),
-            'item_amount' => $fake->numberBetween(100,5000),
+            'item_number' => $fake->numberBetween(1, 999),
+            'item_amount' => $fake->numberBetween(100, 5000),
             'published' => $fake->dateTime('now'),
             'created_at' => $fake->dateTime('now'),
             'updated_at' => $fake->dateTime('now'),
